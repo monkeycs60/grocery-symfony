002a1546038894fde1e6115c08c85fb7f06de6d9
monkeycs60 <clement.serizay@gmail.com>
2024-01-18 18:54:48 +0100

    fix: implementing themes and stable bootstrap

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-import './bootstrap.js';
+import './theme.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -6,5 +6,7 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.scss';
+
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
