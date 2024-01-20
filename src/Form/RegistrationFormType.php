2108bc1e2f0c0bbbdd5400cd2cd8e65e5a1eef40
monkeycs60 <clement.serizay@gmail.com>
2024-01-20 18:56:43 +0100

    style: finishing styling register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -30,6 +30,7 @@ class RegistrationFormType extends AbstractType
             ],
         ])
             ->add('agreeTerms', CheckboxType::class, [
+                'label' => false,
                                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
@@ -40,6 +41,7 @@ class RegistrationFormType extends AbstractType
             ->add('plainPassword', PasswordType::class, [
                                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'label' => "Mot de passe",
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
